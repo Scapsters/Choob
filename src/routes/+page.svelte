fb7c6dcc44b3899b9f9484052587f7ac41d3fa80
Jung-Choi-01 <jungchoi2023+github.com@gmail.com>
2026-06-28 10:21:25 -0700

    wip broken lichess oauth

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,23 @@
 	import { Chess } from 'chess.js';
 	import 'svelte5-chessground/style.css';
 	import ChessBoard from '../components/ChessBoard.svelte';
+	import { Login, authToken } from './login.svelte.ts'
+	import { page } from '$app/state';
+	import { onMount } from 'svelte';
+
+
+	let login: Login;
+	onMount(() => {
+		login = new Login("https://redirectmeto.com/"+page.url.href);
+		login.init();
+	});
 
 	const chess = new Chess();
 </script>
 
+
+<button onclick={() => login.login()}>
+bello
+</button>
+<p><b>Access token:</b> {authToken?.token ?? 'Not logged in'}</p>
 <ChessBoard {chess} />
---
+++ src/routes/login.svelte.ts
@@ -0,0 +1,72 @@
+import { OAuth2AuthCodePKCE } from '@bity/oauth2-auth-code-pkce';
+import type { AccessContext } from '@bity/oauth2-auth-code-pkce';
+
+
+export const lichessHost = 'https://lichess.org';
+export const scopes = ['study:read'];
+export const clientId = 'choob.com';
+export const authToken = $state({
+	token: undefined
+});
+
+export class Login {
+  redirectUrl: string;
+  oauth: OAuth2AuthCodePKCE;
+  error?: any;
+  accessContext?: AccessContext;
+
+  constructor(redirectUrl: string) {
+    this.redirectUrl = redirectUrl;
+    this.oauth = new OAuth2AuthCodePKCE({
+      authorizationUrl: `${lichessHost}/oauth`,
+      tokenUrl: `${lichessHost}/api/token`,
+      clientId,
+      scopes,
+      redirectUrl: this.redirectUrl,
+      onAccessTokenExpiry: refreshAccessToken => refreshAccessToken(),
+      onInvalidGrant: _retry => {},
+    });
+  }
+
+  async login() {
+    // Redirect to authentication prompt.
+    await this.oauth.fetchAuthorizationCode();
+  }
+
+  async init() {
+    try {
+      const hasAuthCode = await this.oauth.isReturningFromAuthServer();
+      if (hasAuthCode) {
+        // Might want to persist accessContext.token until the user logs out.
+        this.accessContext = await this.oauth.getAccessToken();
+
+        console.log('Received token: ' + this.accessContext);
+
+        // Can also use this convenience wrapper for fetch() instead of
+        // using manually using getAccessToken() and setting the
+        // "Authorization: Bearer ..." header.
+        const fetch = this.oauth.decorateFetchHTTPClient(window.fetch);
+      }
+    } catch (err) {
+      this.error = err;
+    }
+  }
+
+  async logout() {
+    const token = this.accessContext?.token?.value;
+    this.accessContext = undefined;
+    this.error = undefined;
+
+    // Example request using vanilla fetch: Revoke access token.
+    await fetch(`${lichessHost}/api/token`, {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  }
+
+  getAccessTokenValue(): string | undefined {
+    return this.accessContext?.token?.value;
+  }
+}
