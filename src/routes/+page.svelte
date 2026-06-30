16082e9ceca073557c4dc42d96529aa7aa5e37fc
Jung-Choi-01 <jungchoi2023+github.com@gmail.com>
2026-06-29 18:00:40 -0700

    states & sliders for weights/studyid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,20 +17,35 @@
 	});
 
 	const chess = new SvelteChess();
-	
-	let playerColor: Color = 'w'
-
-	$effect(() => {
-		if (chess.turn !== playerColor) {
-			getStudyMove('mzJ7q0W7', chess.fen).then(moves => {
-				chess.move(moves[Math.floor(Math.random() * moves.length)].notation.notation)
-			})
-		}
-	})
-
+	let studyId = $state('mzJ7q0W7');
+	let studyIsPublic = $state(true);
+	let weightCommonMove = $state(50);
+	let weightStudyMove = $state(50);
+	let weightEngineMove = $state(0);
 </script>
 
 <button onclick={() => login.login()}> bello </button>
 <p><b>Access token:</b> {authToken?.token?.value || 'Not logged in'}</p>
 <ChessBoard {chess} />
+<p>
+	Study ID: <input bind:value={studyId} placeholder="Input study Id..." />
+	Study is public? <input type="checkbox" bind:checked={studyIsPublic} />
+</p>
+
+<p>
+	Study move weight: 
+	<input type="number" bind:value={weightStudyMove} min="0" max="100" />
+	<input type="range" bind:value={weightStudyMove} min="0" max="100" />
+</p>
+<!-- TODO: force common/engine weight to 0 if authtoken is null, remove check in onMove -->
+<p>
+	Common move weight: 
+	<input type="number" bind:value={weightCommonMove} min="0" max="100" />
+	<input type="range" bind:value={weightCommonMove} min="0" max="100" />
+</p>
+<p>
+	Engine move weight: 
+	<input type="number" bind:value={weightEngineMove} min="0" max="100" />
+	<input type="range" bind:value={weightEngineMove} min="0" max="100" />
+</p>
 <p>I am the mini board: {chess.fen}</p>
