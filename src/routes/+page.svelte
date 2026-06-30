4206cba77692873b44a8ba055e4735701d65b431
Jung-Choi-01 <jungchoi2023+github.com@gmail.com>
2026-06-29 18:01:50 -0700

    choose and perform move based on weights

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,17 @@
 	import { SvelteURL } from 'svelte/reactivity';
 	import type { Color } from 'chess.js';
 	import { getStudyMove } from '../lib/chess/getStudyMove.ts';
+	import type { _ } from '$env/static/private';
+	import Chooser from '../lib/external-packages/Chooser.js';
+	import { getCommonMove } from '../lib/chess/getCommonMove.ts';
+	import { getUCIHistory } from '../lib/chessjs-uci.ts';
+	import { getEngineEvaluation } from '../lib/chess/getEngineEvaluation.ts';
+
+	type MoveType = 'study' | 'common' | 'engine';
+	type MoveWeight = {
+		type: MoveType;
+		weight: number;
+	};
 
 	let login: Login;
 	onMount(() => {
@@ -17,16 +28,84 @@
 	});
 
 	const chess = new SvelteChess();
+	let playerColor: Color = 'w';
+
 	let studyId = $state('mzJ7q0W7');
 	let studyIsPublic = $state(true);
 	let weightCommonMove = $state(50);
 	let weightStudyMove = $state(50);
 	let weightEngineMove = $state(0);
+	let weights: MoveWeight[] = $derived([
+		{
+			type: 'study',
+			weight: weightStudyMove
+		},
+		{
+			type: 'common',
+			weight: weightCommonMove
+		},
+		{
+			type: 'engine',
+			weight: weightEngineMove
+		}
+	]);
+
+	let onMove = $derived(async () => {
+		// board should only move on its own when it's not our turn
+		if (chess.turn === playerColor) return;
+
+		// study moves are the only viable option if authtoken is not set
+		if (authToken?.token?.value === undefined) {
+			console.warn('authToken has not been set, defaulting to always using study moves');
+			let moves = await getStudyMove(studyId, chess.fen, authToken?.token?.value, studyIsPublic);
+			if (moves?.length) {
+				chess.move(moves[Math.floor(Math.random() * moves.length)].notation.notation);
+			} else {
+				throw new Error(
+					'no API key provided but not study move found. cannot move! should end the game in this state later'
+				);
+			}
+			return;
+		}
+
+		// otherwise, use the weights. fall through to the next case if any step returns none
+		switch (Chooser.chooseWeightedObject(weights).type as MoveType) {
+			case 'study':
+				console.log("Trying study move");
+				let studyMoves = await getStudyMove(studyId, chess.fen, authToken?.token?.value, studyIsPublic);
+				if (studyMoves?.length) {
+					let studyMove = studyMoves[Math.floor(Math.random() * studyMoves.length)].notation.notation;
+					console.log(`Using study move: ${JSON.stringify(studyMove)}`)
+					chess.move(studyMove);
+					break;
+				}
+			case 'common':
+				console.log("Trying common move");
+				let commonMove = await getCommonMove(authToken?.token?.value, { play: getUCIHistory(chess) });
+				if (commonMove?.length) {
+					console.log(`Using common move: ${JSON.stringify(commonMove)}`)
+					chess.move(commonMove);
+					break;
+				}
+			case 'engine':
+				console.log("Trying engine move");
+				let engineMove = await getEngineEvaluation(chess.fen, authToken?.token?.value);
+				if(engineMove) {
+					console.log(`Using engine move: ${JSON.stringify(engineMove.move)}`)
+					chess.move(engineMove.move)
+				} else {
+					throw new Error(
+						'Unable to resolve move as study, common, nor engine, should end game in this state later'
+					);
+				}
+				break;
+		}
+	});
 </script>
 
 <button onclick={() => login.login()}> bello </button>
 <p><b>Access token:</b> {authToken?.token?.value || 'Not logged in'}</p>
-<ChessBoard {chess} />
+<ChessBoard {chess} {onMove} />
 <p>
 	Study ID: <input bind:value={studyId} placeholder="Input study Id..." />
 	Study is public? <input type="checkbox" bind:checked={studyIsPublic} />
