f301bdb0931a10437e509c696af2a47f321cdb35
Jung-Choi-01 <jungchoi2023+github.com@gmail.com>
2026-06-29 17:55:56 -0700

    add util for chessjs history -> UCI

---
+++ src/lib/chessjs-uci.ts
@@ -0,0 +1,13 @@
+import { SvelteChess } from '../components/ChessBoard.svelte';
+import type { Move } from 'chess.js'
+
+/**
+ * Given a SvelteChess, return its history in UCI
+ * Basically a wrapper on .history({ verbose: true })
+ * @param chess the SvelteChess object to extract history from
+ */
+export function getUCIHistory(chess: SvelteChess): string {
+    return (chess.historyVerbose() as Move[]).map(move =>
+        move.from + move.to + (move.promotion || "")
+    ).join(",");
+}
