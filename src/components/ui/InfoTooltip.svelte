16cb823db9555b877cc1e577bd6a74f8e9d31d50
Scapsters <scotty.happy@gmail.com>
2026-07-27 22:23:40 -0400

    lower tooltip

--- src/components/ui/InfoTooltip.svelte
+++ src/components/ui/InfoTooltip.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <span {title}>
-    <div class="font-xs text-(--foreground-gray) -translate-y-1/4 bg-(--disabled-color)/25 rounded-full h-5 w-5 text-center">
-        <div class="curly -translate-y-1/8 select-none">i</div>
+    <div class="font-xs text-(--foreground-gray)/60 bg-(--disabled-color)/25 rounded-full h-5 w-5 text-center">
+        <div class="curly font-bold -translate-y-1/8 select-none">i</div>
     </div>
 </span>
 
